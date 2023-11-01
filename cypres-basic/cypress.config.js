e1219827116e63d9ae73499f90ba3f222adeaa0f
shintanur <shintan081@gmail.com>
2023-11-01 11:52:48 +0700

    ,.,

--- cypres-basic/cypress.config.js
+++ cypres-basic/cypress.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // konfigurasi global
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  screenshotOnRunFailure: true,
+  video: true,
+  watchForFileChanges: true,
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
+  experimentalStudio: true,
+
+  // konfigurasi E2E
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
